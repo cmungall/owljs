fbe9b48d4a22baaaff7e9d207f5b72bf13b7bbc1
Chris Mungall <cjm@berkeleybop.org>
2014-01-22 14:05:58 -0800

    removed old

--- test/bootcaro.js
+++
@@ -1,5 +0,0 @@
-// CL-specific setup
-owl.config.idspace = 'CL';
-owl.config.lastId = 10000; // CJM
-owl.defaultSlotMap = { created_by : "GOC:cjm" }; // todo - dates
-owl.config.defaultFormat = new org.semanticweb.owlapi.io.OWLFunctionalSyntaxOntologyFormat(); // todo - introspect this
