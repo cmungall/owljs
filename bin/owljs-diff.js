fbe494649591f4393b179ec1a67d41b7818870f1
Chris Mungall <cjm@berkeleybop.org>
2014-02-04 18:24:16 -0800

    docs

--- bin/owljs-diff.js
+++ bin/owljs-diff.js
@@ -11,16 +11,18 @@ function main(args) {
     var grepFunc = null;
 
     parser.addOption('h', 'help', null, 'Display help');
-    parser.addOption('r', 'reasoner', 'Reasoner', 'set reasoner factory. Default is elk.');
+    parser.addOption('r', 'reasoner', 'Reasoner', 'set reasoner factory. Default is elk. NOT IMPLEMENTED');
     parser.addOption('o', 'outputFile', 'File', 'output file (defaults to stdout)');
 
     var options = parser.parse(args);
 
     if (options.help) {
-        print("Usage: owljs-owldiffer OPTIONS OWLFILE_A OWLFILE_B\n");
-        print("Differ");
+        print("Usage: owljs-owldiff OPTIONS OWLFILE_A OWLFILE_B\n");
+        print("Writes differences between two files. Current output is markdown");
         print("\nOptions:");
 	print(parser.help());
+        print("\nExample:");
+        print("owljs-diff -o diff.md ont1.owl ont2.owl");
 	system.exit('-1');
     }
 
